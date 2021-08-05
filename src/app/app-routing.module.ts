7bc00d4bedeb26bbd0408b4748182297e2bba824
Joseph Axel AKE-DANHO <josephaxelad@gmail.com>
2021-08-05 11:39:19 +0100

    feat (home) : ajout component home
    ajout component + route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'accueil', component:  HomeComponent},
+  { path: '', pathMatch: 'full', redirectTo: 'accueil' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
