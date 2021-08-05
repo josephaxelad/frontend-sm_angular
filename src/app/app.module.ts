432e65d76155bea8007d7bf1708b041b8813770c
Joseph Axel AKE-DANHO <josephaxelad@gmail.com>
2021-08-05 11:59:47 +0100

    feat (projet) : ajout de header + footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,23 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FlexLayoutModule } from '@angular/flex-layout';
+<<<<<<< Updated upstream
 
 @NgModule({
   declarations: [
     AppComponent
+=======
+import { HomeComponent } from './components/home/home.component';
+import { HeaderComponent } from './components/partials/header/header.component';
+import { FooterComponent } from './components/partials/footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent
+>>>>>>> Stashed changes
   ],
   imports: [
     BrowserModule,
