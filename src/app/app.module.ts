bef7bd38f61f9ed62e7f015c4c8b6ff1a98ac523
Joseph Axel AKE-DANHO <josephaxelad@gmail.com>
2021-08-09 22:35:10 +0100

    feat (home) : ajout component slider-home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 import { HomeComponent } from './components/home/home.component';
 import { HeaderComponent } from './components/partials/header/header.component';
 import { FooterComponent } from './components/partials/footer/footer.component';
+import { SliderHomeComponent } from './components/home/slider-home/slider-home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    SliderHomeComponent
   ],
   imports: [
     BrowserModule,
