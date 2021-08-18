19c8b26096ce3cc727615cf6f9d6a690ff545cde
Joseph Axel AKE-DANHO <josephaxelad@gmail.com>
2021-08-18 21:02:36 +0100

    feat (shop) : ajout component shop

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { ShopComponent } from './components/shop/shop.component';
 
 const routes: Routes = [
   { path: 'accueil', component:  HomeComponent},
+  { path: 'boutique', component:  ShopComponent},
   { path: '', pathMatch: 'full', redirectTo: 'accueil' },
 ];
 
