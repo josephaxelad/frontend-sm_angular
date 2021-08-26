70b7c51b0d1aecf3951188e3553daff124e81857
Joseph Axel AKE-DANHO <josephaxelad@gmail.com>
2021-08-26 21:56:48 +0100

    feat (blog) : ajout component + routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BlogComponent } from './components/blog/blog.component';
 import { HomeComponent } from './components/home/home.component';
 import { ShopComponent } from './components/shop/shop.component';
 
 const routes: Routes = [
   { path: 'accueil', component:  HomeComponent},
   { path: 'boutique', component:  ShopComponent},
+  { path: 'blog', component:  BlogComponent},
   { path: '', pathMatch: 'full', redirectTo: 'accueil' },
 ];
 
